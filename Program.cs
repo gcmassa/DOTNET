ba7721801b3e52a3a58279a8b77bdc5eab03cff0
GiancarloMassaccesi <gian.massaccesi@gmail.com>
2025-06-22 22:24:37 -0400

    Add Pessoa model with properties and presentation method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("teste");
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("teste");
+using DOTNET.Models;
+
+Pessoa p = new Pessoa();
+
+p.Nome = "Severino";
+p.Idade = 50;
+p.Apresentar();
--- obj/Debug/net9.0/DOTNET.AssemblyInfo.cs
+++ obj/Debug/net9.0/DOTNET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DOTNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d49ea752fddd81bc0f4ce0393cc36f043b4ec73")]
 [assembly: System.Reflection.AssemblyProductAttribute("DOTNET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DOTNET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
