1591b51c0d7eae295ae48f54b8f7e53192094148
GiancarloMassaccesi <gian.massaccesi@gmail.com>
2025-07-03 13:30:37 -0400

    Refactor Pessoa model and update presentation method; add date display in Program.cs

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -9,10 +9,15 @@ namespace DOTNET.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+        
+        //public string NomeRepresentanteLegalDaPessoaFisica { get; set; }
 
         public void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+            //Console.WriteLine($"Olá, meu nome é " + 
+            // $"{Nome}, e tenho {Idade} anos");
+            //Console.WriteLine($"Olá, meu nome é {Nome}\n e tenho {Idade} anos");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,8 +3,41 @@
 //Console.WriteLine("teste");
 using DOTNET.Models;
 
-Pessoa p = new Pessoa();
 
-p.Nome = "Severino";
-p.Idade = 50;
-p.Apresentar();
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+/*string apresentacao = "Olá, seja bem vindo";
+
+
+
+int quantidade = 1;
+
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+quantidade = 10;
+
+Console.WriteLine("Valor da variável quantidade: " + quantidade);*/
+
+/*
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condicao = true;*/
+/*
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00")); //ToString sendo utilizado para tratar a casa depois da virgula no caso da altura
+Console.WriteLine("Valor da variável preço: " + preco);
+Console.WriteLine("Valor da variável condição: " + condicao);*/
+
+
+
+//Pessoa p = new Pessoa();
+
+//p.Nome = "Severino";
+//p.Idade = 50;
+//p.Apresentar();
