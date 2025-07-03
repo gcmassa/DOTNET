64acf99ab0cc21fe7dbb5557db4b7ab7f1f755c9
GiancarloMassaccesi <gian.massaccesi@gmail.com>
2025-07-03 13:35:36 -0400

    Make Nome property nullable in Pessoa model

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,7 +7,7 @@ namespace DOTNET.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public int Idade { get; set; }
         
         //public string NomeRepresentanteLegalDaPessoaFisica { get; set; }
